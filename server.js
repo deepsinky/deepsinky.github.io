2bc848b0e24bcc424c42e699dc7dbfeeef76d0ba
deepsinky <deepsinky@gmail.com>
2026-03-20 01:18:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ try{
 const message = req.body.message;
 
 const response = await fetch(
-`https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${API_KEY}`,
+`https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${process.env.API_KEY}`,
 {
 method:"POST",
 headers:{
@@ -27,7 +27,6 @@ contents:[{parts:[{text:message}]}]
 })
 }
 );
-
 const data = await response.json();
 
 const reply =
