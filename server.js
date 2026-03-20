ab13997ed3a83f1cf0ac891ff9de11a6ad35720f
deepsinky <deepsinky@gmail.com>
2026-03-21 00:16:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ console.log("API KEY CHECK:", API_KEY ? "OK" : "MISSING");
 app.post("/chat", async (req, res) => {
   try {
     const message = req.body.message;
-
+console.log("USER MESSAGE:", message);
     const response = await fetch(
       `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`,
       {
