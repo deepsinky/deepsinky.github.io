050307089a68b6ecea3a0386f60f0098a3adf940
deepsinky <deepsinky@gmail.com>
2026-03-20 09:43:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,9 +14,8 @@ app.post("/chat", async (req,res)=>{
 try{
 
 const message = req.body.message;
-
 const response = await fetch(
-`https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${process.env.API_KEY}`,
+`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.API_KEY}`,
 {
 method:"POST",
 headers:{
@@ -27,6 +26,7 @@ contents:[{parts:[{text:message}]}]
 })
 }
 );
+
 const data = await response.json();
 
 const reply =
